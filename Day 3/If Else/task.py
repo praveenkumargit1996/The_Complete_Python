b9aceb01ed00e5d86e9c22c2ce16a341546806bf
praveenkumargit1996 <praveenkb1996@gmail.com>
2026-08-06 12:09:56 +0530

    day3 conditional stms and operators tasks

---
+++ Day 3/If Else/task.py
@@ -0,0 +1,15 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("sorry you cannot ride the rollercoaster")
+
+
+if height != 120:
+    print("sorry you cannot ride the rollercoaster")
+
+
+if height == 120:
+    print("You can ride the rollercoaster")
---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,8 @@
+#Even_number 12 % 2 == 0
+
+number_to_check = int(input("what is the number you want to check?"))
+
+if number_to_check % 2 == 0 :
+    print("Even")
+else:
+    print("Odd")
