bb9804d2201e0da9f9e6e015bb97148ad8d7b690
praveenkumargit1996 <praveenkb1996@gmail.com>
2026-08-13 03:08:25 +0530

    day6 functions and while loops

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,47 @@
+
+# defining user-defined functions
+def my_function():
+    print("Hello")
+    print("Praveen")
+
+# calling the function
+my_function()
+
+
+# robot game of hardle
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_around()
+#     move()
+#     turn_around()
+#     move()
+#     turn_left()
+#
+# for step in range(6):
+#  jump()
+
+# for loops
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+for n in range(1,6):
+    print(n)
+
+# while loop
+num_of_hurdals =6
+
+def jump():
+    print("jump")
+
+while not num_of_hurdals>0:
+    jump()
+    num_of_hurdals -= 1
+
