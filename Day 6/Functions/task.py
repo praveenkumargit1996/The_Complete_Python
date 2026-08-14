3a6f36ebf1eb2b8f458e086aab4fe51f108f3f3d
praveenkumargit1996 <praveenkb1996@gmail.com>
2026-08-14 20:40:54 +0530

    day6 functions and while loops changes

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -7,26 +7,6 @@ def my_function():
 # calling the function
 my_function()
 
-
-# robot game of hardle
-# def turn_around():
-#     turn_left()
-#     turn_left()
-#     turn_left()
-#
-# def jump():
-#     move()
-#     turn_left()
-#     move()
-#     turn_around()
-#     move()
-#     turn_around()
-#     move()
-#     turn_left()
-#
-# for step in range(6):
-#  jump()
-
 # for loops
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
@@ -45,3 +25,87 @@ while not num_of_hurdals>0:
     jump()
     num_of_hurdals -= 1
 
+#Robot hardle challenge Hadle 1- https://reeborg.ca/reeborg.html?
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_around()
+#     move()
+#     turn_around()
+#     move()
+#     turn_left()
+#
+# for step in range(6):
+#  jump()
+
+#Robot hardle challenge Hadle 3- https://reeborg.ca/reeborg.html?
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# while not at_goal():
+#   if wall_in_front():
+#      jump()
+#   else :
+#     move()
+
+#Robot hardle challenge Hadle 4- https://reeborg.ca/reeborg.html?
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#          move()
+#     turn_left()
+#
+# while not at_goal():
+#   if wall_in_front():
+#      jump()
+#   else :
+#     move()
+
+
+#Robot hardle challenge hard level maze - https://reeborg.ca/reeborg.html?
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def moveforward():
+#     while front_is_clear() and not at_goal():
+#         move()
+#         if not wall_on_right():
+#             turn_right()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         turn_left()
+#     elif front_is_clear():
+#         moveforward()
